320567439f93c88b511ee980e8022a1178c7be64
Riley Wheadon <rileywheadon@gmail.com>
2024-11-22 11:58:20 -0800

    Add admin dashboard, begin collecting poll response data.

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from app import app, db
 from models import *
 
@@ -23,6 +23,52 @@ def home():
     # Query the database to get all polls
     polls = db.session.execute(db.select(Poll)).scalars().all()
 
-    # Renderthe template
+    # Render the template
     return render_template("home.html", user=user, polls=polls)
 
+
+# HTTP endpoint for responding to polls
+@app.route("/home/respond", methods = ["GET", "POST"])
+def respond():
+
+    # Get the user ID and answer ID from the request
+    user_id = request.args.get('user_id')
+    answer_id = request.args.get('answer_id')
+
+    # Get the poll ID byq uerying the database 
+    answer = PollAnswer.query.get(answer_id)
+    poll_id = Poll.query.get(answer.poll_id).id
+
+    # Create a new Response and add it to the database
+    response = Response(user_id = user_id, poll_id = poll_id, answer_id = answer_id)
+    db.session.add(response)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+# Admin endpoint for viewing the responses
+@app.route("/admin")
+def admin():
+
+    # Get the responses from the database
+    responses = db.session.execute(db.select(Response)).scalars().all()
+    print(responses)
+
+    # Render the admin dashboard
+    return render_template("admin.html", responses=responses)
+
+# Routes for creating/deleting/updating polls (ADMIN ONLY)
+@app.route("/admin/createpoll", methods = ["GET", "POST"])
+def create_poll():
+    return redirect(url_for("home"))
+
+@app.route("/admin/deletepoll", methods = ["GET", "POST"])
+def delete_poll():
+    return redirect(url_for("home"))
+
+@app.route("/admin/createanswer", methods = ["GET", "POST"])
+def create_answer():
+    return redirect(url_for("home"))
+
+@app.route("/admin/deleteanswer", methods = ["GET", "POST"])
+def delete_answer():
+    return redirect(url_for("home"))
