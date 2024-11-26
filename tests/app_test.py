127da6562fad6f57006d9013af65d417db49a2d1
Riley Wheadon <rileywheadon@gmail.com>
2024-11-26 11:39:05 -0800

    Improve admin UX. Style home page. Add HTMX and TailwindCSS.

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,16 @@
+from polll.models import User, Poll, PollAnswer, Response
+
 def test_landing(client):
     response = client.get("/")
     assert b"<h1>Welcome to Polll!</h1>" in response.data
+
+def test_home(client, db):
+    u = User(username = 'testuser', email = 'test@test.com')
+    db.session.add(u)
+    db.session.commit()
+
+    response = client.get("/home")
+    assert b"<h1>testuser</h1>" in response.data 
+    assert b"<h2>test@test.com</h2>" in response.data 
+
 
