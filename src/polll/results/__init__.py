b0158d186e555811e1d03c1d9f8e1b9274ef74d4
Riley Wheadon <rileywheadon@gmail.com>
2024-12-04 19:30:11 -0800

    Add visualization for results.

---
+++ src/polll/results/__init__.py
@@ -0,0 +1,8 @@
+from .choose_one import choose_one
+from .choose_many import choose_many
+from .numeric_star import numeric_star
+from .numeric_scale import numeric_scale
+from .ranked_poll import ranked_poll
+from .tier_list import tier_list
+
+
--- src/polll/static/tailwind.config.js
+++ src/polll/static/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   content: ["../templates/**"],
+  blocklist: [
+    'main-svg', // Ensure Plotly's classes are not purged
+  ],
   theme: {
     extend: {},
   },
