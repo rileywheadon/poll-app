92ed47ae49047f994534c99191a654f58c315e3c
MyNamesMatt28 <mattdehaas28@gmail.com>
2024-12-30 02:06:24 -0500

    add new/hot/top buttons on the feed page

--- src/polll/static/scripts/ui.js
+++ src/polll/static/scripts/ui.js
@@ -64,3 +64,20 @@ function translate_home_sidebar() {
     document.getElementById("hmbr-btn").classList.toggle("translate-x-8");
 }
 
+function toggle_filter_btn(col, id) {
+    const buttons = [document.getElementById("New"),
+                     document.getElementById("Hot"),
+                     document.getElementById("Top")];
+    buttons.forEach((b) => {
+        b.classList.remove("bg-nord-9");
+        b.classList.remove("dark:bg-nord-7");
+        b.classList.add("bg-nord-4");
+        b.classList.remove("font-semibold");
+        b.classList.add("font-light");
+        });
+    document.getElementById(id).classList.add("bg-nord-9");
+    document.getElementById(id).classList.add("dark:bg-nord-7");
+    document.getElementById(id).classList.remove("font-light");
+    document.getElementById(id).classList.add("font-semibold");
+    // Could put callback function here for the each button based on its id
+}
