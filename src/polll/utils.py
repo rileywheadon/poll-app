Fix the average function for scale graphs and allow for any number of divisions in the linspace function.

src/polll/utils.py
@@ -17,9 +17,10 @@ OUTPUT: List of two parallel lists representing (x, y) points
 
 
 def smooth_hist(data, bandwidth):
-    # Third parameter helps with local smoothing but anything above
-    # 100 casues a noticable performance drop
-    x_vals = np.linspace(0, 100, 250)
+    # Third parameter (must be above 101) helps with local 
+    # smoothing but anything above 150 casues a noticable
+    # drop in performance
+    x_vals = np.linspace(0, 100, 200)
     adj_data = [[i["value"]] * i["count"] for i in data]
     adj_data = [i for j in adj_data for i in j]
 